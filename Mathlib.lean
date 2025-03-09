8564eed23c3db5f8c090046241fd47291a793f95
Mike Lu <mike020830@gmail.com>
2025-03-08 20:44:53 -0800

    test

--- Mathlib.lean
+++ Mathlib.lean
@@ -6004,3 +6004,19 @@ import Mathlib.Util.TermReduce
 import Mathlib.Util.TransImports
 import Mathlib.Util.WhatsNew
 import Mathlib.Util.WithWeakNamespace
+
+
+
+open Nat (add_assoc add_comm)
+
+theorem hello_world (a b c : Nat)
+  : a + b + c = a + c + b := by
+  -- Step 1: Rewrite using associativity of addition
+  rw [add_assoc]
+  -- Now we have (a + b) + c = a + c + b
+  -- Step 2: Rewrite using commutativity of addition
+  rw [add_comm b c]
+  -- Now we have (a + c) + b = a + c + b
+  -- Step 3: Rewrite using associativity of addition again
+  rw [←add_assoc]
+  -- Now we have a + (c + b) = a + c + b
