e9cf4bc03ba94e9f371fdacd88a5e815ece80ece
Mike Lu <mike020830@gmail.com>
2025-03-08 21:17:52 -0800

    up

--- Mathlib.lean
+++ Mathlib.lean
@@ -56,16 +56,16 @@ import Archive
 
 
 
-open Nat (add_assoc add_comm)
+-- open Nat (add_assoc add_comm)
 
-theorem hello_world (a b c : Nat)
-  : a + b + c = a + c + b := by
-  -- Step 1: Rewrite using associativity of addition
-  rw [add_assoc]
-  -- Now we have (a + b) + c = a + c + b
-  -- Step 2: Rewrite using commutativity of addition
-  rw [add_comm b c]
-  -- Now we have (a + c) + b = a + c + b
-  -- Step 3: Rewrite using associativity of addition again
-  rw [←add_assoc]
-  -- Now we have a + (c + b) = a + c + b
+-- theorem hello_world (a b c : Nat)
+--   : a + b + c = a + c + b := by
+--   -- Step 1: Rewrite using associativity of addition
+--   rw [add_assoc]
+--   -- Now we have (a + b) + c = a + c + b
+--   -- Step 2: Rewrite using commutativity of addition
+--   rw [add_comm b c]
+--   -- Now we have (a + c) + b = a + c + b
+--   -- Step 3: Rewrite using associativity of addition again
+--   rw [←add_assoc]
+--   -- Now we have a + (c + b) = a + c + b
